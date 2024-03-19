da3bd9fc4351780cd48392c23f63777f2802935d
Viktor_Marchuk <viktor.m77.77@mail.ru>
2024-03-19 11:08:30 +0300

    init commit

---
+++ src/main/java/com/vm/cargosearch/CargoSearchApplication.java
@@ -0,0 +1,13 @@
+package com.vm.cargosearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CargoSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CargoSearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vm/cargosearch/CargoSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vm.cargosearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CargoSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
